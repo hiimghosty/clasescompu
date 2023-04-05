ebc8915d8cad6599dfeadf4bf1cd65cac0aa7f6f
Mauri <mauricioeliasbenitez@gmail.com>
2023-04-04 21:03:13 -0400

    tiriririri

---
+++ 2P/ejemplovectores.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int cargaDeVector(){
+    int n;
+    cin>>n;
+}
+
+int main(){
+    int vector[9];
+    for(int i=0; i<9; i++){
+        vector[i]=cargaDeVector;
+        cout<<vector[i];
+    }
+
+
+    return 0;
+}
