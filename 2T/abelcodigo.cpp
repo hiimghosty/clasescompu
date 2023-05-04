b0f207fd51f8c6db36dc6a94accb9584a39dd066
Mauri <mauricioeliasbenitez@gmail.com>
2023-05-04 15:28:44 -0400

    Arreglado

--- 2T/abelcodigo.cpp
+++ 2T/abelcodigo.cpp
@@ -1,27 +1,29 @@
 #include <iostream>
 #include <cstdlib>
 using namespace std;
-//FALTA CORREGIR
+// FALTA CORREGIR
 void impresionDeVector(int vector[], int tam)
 {
-    for (int i = 0; i < 20; i++)
+    for (int i = 0; i < tam; i++)
     {
         cout << vector[i] << " ";
     }
 }
 void generarV(int V[], int n)
 {
-    //cout << "introduzca las componentes del vector:";
+    // cout << "introduzca las componentes del vector:"
+    // Puse que sea aleatorio porque era garra nomas
     for (int i = 0; i < n; i++)
     {
-        V[i]  = 0 + rand() % (100 - 0 + 1);
+        V[i] = 0 + rand() % (100 - 0 + 1);
     }
 }
 
 void cantAparicion(int V[], int n)
 {
-    int cont = 0, x;
-    cout << "introduzca un numero:";
+    int cont = 0;
+    int x = 0;
+    cout << "Introduzca un numero:";
     cin >> x;
     for (int j = 0; j < n; j++)
     {
@@ -29,30 +31,36 @@ void cantAparicion(int V[], int n)
             cont++;
     }
     if (cont != 0)
-        cout << "el numero " << x << " aparecio " << cont << " veces";
+        cout << "El numero " << x << " aparecio " << cont << " veces" << endl;
     else
     {
-        cout << "el numero" << x << "no es componente del vector";
+        cout << "El numero" << x << "no es componente del vector" << endl;
     }
 }
 
 int MenosAparicion(int V[], int n)
 {
-    
+    int elQueMenosAparece = V[0], aparicionesMenosAparece = 1, aparicionesAux = 0;
     for (int i = 0; i < n; i++)
     {
-        int cont1 = 0;
+
         for (int j = 0; j < n; j++)
         {
-            if (V[i] == V[j] and i != j)
-                cont1++;
+            if (V[i] == V[j])
+                aparicionesAux++;
+            else
+                continue;
         }
-        if (cont1 == 0)
+        if (aparicionesAux < aparicionesMenosAparece)
         {
-            return V[i];
-            break;
+            aparicionesMenosAparece = aparicionesAux;
+            elQueMenosAparece = V[i];
         }
+
+        aparicionesAux = 0;
     }
+
+    return elQueMenosAparece;
 }
 
 bool estaOrdenado(int V[], int n)
@@ -77,13 +85,14 @@ bool estaOrdenado(int V[], int n)
 int main()
 {
     int n;
-    int V[n];
-    cout << "introduzca el numero de componentes del vector:";
+    cout << "Introduzca el numero de componentes del vector:";
     cin >> n;
+    int V[n];
     generarV(V, n);
-    impresionDeVector(V,n);
+    impresionDeVector(V, n);
     cantAparicion(V, n);
-   // cout << MenosAparicion(V, n);
+    cout << endl;
+    cout << "El elemento que menos aparece es" << MenosAparicion(V, n);
     if (estaOrdenado(V, n) == true)
         cout << "true";
     else
