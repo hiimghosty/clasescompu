838a6cc27b887f484cbc0e296bded032e9e23481
Mauri <mauricioeliasbenitez@gmail.com>
2023-05-21 23:52:45 -0400

    Clase extra

--- 2T/matrizfuncion.cpp
+++ 2T/matrizfuncion.cpp
@@ -2,16 +2,29 @@
 #include <cstdlib>
 
 using namespace std;
-//RESOLVER CON FUNCIONES
-void menorElementoFila(int A[], int m)
+void menorElementoFila(int A[], int m, int mPrecios[], int mProveedores[], int contador)
 {
-    cout << "La fila es: " << endl;
+    int Precio = A[0];
+    int Proveedor = 0;
+
     for (int i = 0; i < m; i++)
     {
-        cout << A[i] << " ";
+        if (A[i] < Precio)
+        {
+            Precio = A[i];
+            Proveedor = i;
+        }
+    }
+    mPrecios[contador] = Precio;
+    mProveedores[contador] = Proveedor+1;
+}
+void impresionDeVector(int vector[], int tam)
+{
+    for (int i = 0; i < tam; i++)
+    {
+        cout << vector[i] << " ";
     }
 }
-
 int main()
 {
     int m, n;
@@ -30,7 +43,7 @@ int main()
         }
     }
     cout << endl
-         << "La matriz entera es: " << endl;
+         << "La matriz de precios y proveedores entera es: " << endl;
     // Imprimir la matriz
     for (int i = 0; i < m; i++)
     {
@@ -40,7 +53,16 @@ int main()
         }
         cout << endl;
     }
+    int mPrecios[m];     // Areglo con los mejores precios
+    int mProveedores[m]; // Arreglo con los mejores proveedores
+    for (int i = 0; i < m; i++)
+    {
 
-    menorElementoFila(A[0], m);
+        menorElementoFila(A[i], m, mPrecios, mProveedores, i);
+    }
+    cout << "Los mejores precios son: " << endl;
+    impresionDeVector(mPrecios, m);
+    cout << "Los mejores proveedores son: " << endl;
+    impresionDeVector(mProveedores, m);
     return 0;
 }
