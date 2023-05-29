eff68789ea103f20021479039db111941c31c4ff
Mauri <mauricioeliasbenitez@gmail.com>
2023-05-29 09:46:31 -0400

    dfgd

--- 2T/01bubblesort.cpp
+++ 2T/01bubblesort.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 using namespace std;
 
-
 void imprimirVector(int N[], int tam)
 {
 
@@ -11,35 +10,34 @@ void imprimirVector(int N[], int tam)
     }
 }
 
-void ordenarBurbuja(int A[], int tam){
-//COMPLETAR
-    for ( int i = 0; i < (tam - 1) ; i++){
-        for (int j = 0; j < (tam - 1) ; j++){
-            if (A[j] > A[j+1]){
+void ordenarBurbuja(int A[], int tam)
+{
+    // COMPLETAR
+    for (int i = 0; i < (tam - 1); i++)
+    {
+        for (int j = 0; j < (tam - 1); j++)
+        {
+            if (A[j] > A[j + 1])
+            {
                 int aux = A[j];
-                A[j] = A[j+1];
-                A[j+1] = aux;
-
+                A[j] = A[j + 1];
+                A[j + 1] = aux;
             }
         }
-        imprimirVector(A,tam);
         cout << endl;
     }
-
 }
 
+int main()
+{
 
-
-
-int main () {
-
-    int A[] = {1,2,3,4,5,6,8,7};
+    int A[] = {1, 2, 3, 4, 5, 6, 8, 7};
 
     int tam = 8;
-    int x=5;
-    ordenarBurbuja(A,tam);
+    int x = 5;
+    ordenarBurbuja(A, tam);
     cout << "El vector finalmente ordenado es: " << endl;
-    imprimirVector(A,tam); 
-    //imprime ordenado o no?
+    imprimirVector(A, tam);
+    // imprime ordenado o no?
     return 0;
 }
